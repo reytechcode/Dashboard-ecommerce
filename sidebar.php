25138628bcc4965c3f76d75d73ba363efdf64ea7
devR <73724930+reytechcode@users.noreply.github.com>
2024-08-23 00:19:49 -0500

    Dashboard ecommerce

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,13 @@
-<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+<div class="sidebar border-right col-md-3 col-lg-2 p-0 bg-body-tertiary" data-bs-theme="dark">
+    <div class="offcanvas-md offcanvas-end bg-body-tertiary vh-100" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
         <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+            <h5 class="offcanvas-title text-white" id="sidebarMenuLabel">Company name</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="#">
+                    <a class="nav-link text-white d-flex align-items-center gap-2" aria-current="page" href="#">
                         <i class="fas fa-dashboard"></i>
                         Dashboard
                     </a>
@@ -16,7 +16,7 @@
             <hr class="my-3">
             <ul class="nav flex-column mb-auto">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link text-white d-flex align-items-center gap-2" href="#">
                         <svg class="bi">
                             <use xlink:href="#door-closed" />
                         </svg>
